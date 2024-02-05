bae8e6116522ad1ca8da831a8e6647cb0ad0df7f
Jasmin In-naka <jasmininnaka@gmail.com>
2024-02-06 03:12:45 +0800

    initial

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-medium" style={{ color: '#333' }}>Hey👋 I am Jasmin In-naka</h1>
+    </main>
+  );
+}
