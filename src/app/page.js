018df04874a9ab38fb5b27d4a593eca7ce5c7fa3
Jasmin In-naka <jasmininnaka@gmail.com>
2024-02-09 17:38:51 +0800

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ library.add(fab);
 
 export default function Home() {
   return (
-    <main className="quicksand flex items-center justify-center min-h-screen p-24 gap-5" style={{ background: '#f7f5f5', color: '#555', fontSize: '18px' }}>
+    <main className="quicksand flex min-h-screen p-24 gap-5" style={{ background: '#f7f5f5', color: '#555', fontSize: '18px' }}>
       <div className="flex-1">
-        <h1 className="font-bold uppercase" style={{color: '#57535f', fontSize: '3rem'}}>Hola!👋🏼 I`m Jasmin In-naka </h1>
+        <h1 className="font-bold uppercase text-center" style={{color: '#57535f', fontSize: '3rem'}}>Hola!👋🏼 I`m Jasmin In-naka </h1>
         
       </div>
 
